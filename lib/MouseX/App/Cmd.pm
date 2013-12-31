c3651302c2ae276c105d294919ea755ab5e1fcd0
Mark Gardner <mjgardner@cpan.org>
2013-12-31 14:36:35 +0000

    Import of MJGARDNER/MooseX-App-Cmd-0.27 from CPAN.
    gitpan-cpan-distribution: MooseX-App-Cmd
    gitpan-cpan-version:      0.27
    gitpan-cpan-path:         MJGARDNER/MooseX-App-Cmd-0.27.tar.gz
    gitpan-cpan-author:       MJGARDNER
    gitpan-cpan-maturity:     released

--- lib/MouseX/App/Cmd.pm
+++ lib/MouseX/App/Cmd.pm
@@ -3,7 +3,7 @@ use 5.006;
 package MouseX::App::Cmd;
 use Mouse;
 
-our $VERSION = '0.26';    # TRIAL VERSION
+our $VERSION = '0.27';    # VERSION
 use namespace::clean -except => 'meta';
 extends 'MooseX::App::Cmd';
 __PACKAGE__->meta->make_immutable();   ## no critic (RequireExplicitInclusion)
@@ -27,7 +27,7 @@ MouseX::App::Cmd - Mashes up MouseX::Getopt and App::Cmd
 
 =head1 VERSION
 
-version 0.26
+version 0.27
 
 =head1 SYNOPSIS
 
